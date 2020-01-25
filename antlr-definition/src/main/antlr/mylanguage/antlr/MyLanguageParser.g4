4ab62d36b29f86079d40d5e1edb7b78775b7b5d8
Alex Landau <alandau@cs.stanford.edu>
2020-01-25 11:06:45 -0800

    Add some variants of the rule

--- antlr-definition/src/main/antlr/mylanguage/antlr/MyLanguageParser.g4
+++ antlr-definition/src/main/antlr/mylanguage/antlr/MyLanguageParser.g4
@@ -16,11 +16,36 @@ tokens {
   ID
 }
 
+// String to parse: foo<bar>(baz)
+
+// This fails to parse as intended
 expression : ID
   | expression LESS_THAN ID GREATER_THAN LPAREN ID RPAREN
-  | expression LPAREN ID RPAREN
   | expression LESS_THAN expression // < operator
   | expression GREATER_THAN expression // > operator
   | LPAREN expression RPAREN
   ;
-    catch[RecognitionException e] { throw e; }
+
+// This fails to parse as intended
+expression2 : ID
+  | ID LESS_THAN ID GREATER_THAN LPAREN ID RPAREN
+  | expression2 LESS_THAN expression2 // < operator
+  | expression2 GREATER_THAN expression2 // > operator
+  | LPAREN ID RPAREN
+  ;
+
+// This parses as intended
+expression3 : ID
+  | ID LESS_THAN ID GREATER_THAN LPAREN ID RPAREN
+  | ID LESS_THAN ID // < operator
+  | expression3 GREATER_THAN expression3 // > operator
+  | LPAREN ID RPAREN
+  ;
+
+// This parses as intended
+expression4 : ID
+  | ID LESS_THAN ID GREATER_THAN LPAREN ID RPAREN
+  | ID LESS_THAN expression4 // < operator
+  | ID GREATER_THAN expression4 // > operator
+  | LPAREN ID RPAREN
+  ;
--- antlr-definition/src/test/java/ParsingTest.java
+++ antlr-definition/src/test/java/ParsingTest.java
@@ -7,27 +7,76 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
 
+// We want the following rule to be applied:
+// expression LESS_THAN ID GREATER_THAN LPAREN ID RPAREN
+// But instead, the following rule is applied:
+// expression GREATER_THAN expression
+// This seems to be contrary to ANTLR's professed behavior of preferring the earlier rule.
 public class ParsingTest {
-    @Test
-    public void testParsingFunctionCallWithParameters() {
+    private MyLanguageParser getParser() {
         String textToParse = "foo<bar>(baz)";
         MyLanguageLexer lexer = new MyLanguageLexer(CharStreams.fromString(textToParse));
         TokenStream tokens = new CommonTokenStream(lexer);
-        MyLanguageParser parser = new MyLanguageParser(tokens);
+        return new MyLanguageParser(tokens);
+    }
 
+    @Test
+    public void testParsingFunctionCallWithParameters() {
+        MyLanguageParser parser = getParser();
         MyLanguageParser.ExpressionContext expr = parser.expression();
-
-        // We want the following rule to be applied:
-        // expression LESS_THAN cd_types_nonempty GREATER_THAN LPAREN_NO_WS cd_expressions RPAREN
-        // But instead, the following rule is applied:
-        // expression GREATER_THAN expression
-        // This seems to be contrary to ANTLR's professed behavior of preferring the earlier rule.
 
         System.out.println("expression: " + expr.getText());
         System.out.println("LESS_THAN: " + expr.LESS_THAN());
         System.out.println("GREATER_THAN: " + expr.GREATER_THAN());
-        // TODO: Fix this
-        System.out.println("lparen: " + expr.LPAREN());
+        System.out.println("RPAREN: " + expr.LPAREN());
+        System.out.println("RPAREN: " + expr.RPAREN());
+        assertNotNull(expr.LESS_THAN());
+        assertNotNull(expr.GREATER_THAN());
+        assertNotNull(expr.LPAREN());
+        assertNotNull(expr.RPAREN());
+    }
+
+    @Test
+    public void testRuleVariant2() {
+        MyLanguageParser parser = getParser();
+        MyLanguageParser.Expression2Context expr = parser.expression2();
+
+        System.out.println("expression: " + expr.getText());
+        System.out.println("LESS_THAN: " + expr.LESS_THAN());
+        System.out.println("GREATER_THAN: " + expr.GREATER_THAN());
+        System.out.println("LPAREN: " + expr.LPAREN());
+        System.out.println("RPAREN: " + expr.RPAREN());
+        assertNotNull(expr.LESS_THAN());
+        assertNotNull(expr.GREATER_THAN());
+        assertNotNull(expr.LPAREN());
+        assertNotNull(expr.RPAREN());
+    }
+
+    @Test
+    public void testRuleVariant3() {
+        MyLanguageParser parser = getParser();
+        MyLanguageParser.Expression3Context expr = parser.expression3();
+
+        System.out.println("expression: " + expr.getText());
+        System.out.println("LESS_THAN: " + expr.LESS_THAN());
+        System.out.println("GREATER_THAN: " + expr.GREATER_THAN());
+        System.out.println("LPAREN: " + expr.LPAREN());
+        System.out.println("RPAREN: " + expr.RPAREN());
+        assertNotNull(expr.LESS_THAN());
+        assertNotNull(expr.GREATER_THAN());
+        assertNotNull(expr.LPAREN());
+        assertNotNull(expr.RPAREN());
+    }
+
+    @Test
+    public void testRuleVariant4() {
+        MyLanguageParser parser = getParser();
+        MyLanguageParser.Expression4Context expr = parser.expression4();
+
+        System.out.println("expression: " + expr.getText());
+        System.out.println("LESS_THAN: " + expr.LESS_THAN());
+        System.out.println("GREATER_THAN: " + expr.GREATER_THAN());
+        System.out.println("LPAREN: " + expr.LPAREN());
         System.out.println("RPAREN: " + expr.RPAREN());
         assertNotNull(expr.LESS_THAN());
         assertNotNull(expr.GREATER_THAN());
